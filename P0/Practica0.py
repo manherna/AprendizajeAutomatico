2be5fa150216b9ca8428b68a08a093f02ae39fb4
Manuel Hernández <manherna@ucm.es>
2018-09-24 23:18:20 +0200

    Esqueleto P0

--- P0/Practica0.py
+++ P0/Practica0.py
@@ -1,1 +1,11 @@
-print("Hola, Mundo!")
+# Practica 0 de AAMD
+# Implementación en python de la integración por el metodo 
+# de Monte Carlo
+
+
+def integra_mc (fun, a, b, num_puntos = 1000):
+    # Randomizar una lista con 1000 puntos en el segmento a, b 
+    # con la función fun. 
+    # Después calcular la integral haciendo la división entre
+    # los puntos debajo de la función y fuera
+    return 0
