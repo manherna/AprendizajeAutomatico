aec89a0175252b17eac02d438ebf5a028ca730e4
mjcguada <mjcguada@gmail.com>
2018-09-26 13:42:10 +0200

    Grafica deseada añadida, funciona en notebook, pero da fallo al importar matplotlib

--- P0/Practica0.py
+++ P0/Practica0.py
@@ -1,7 +1,33 @@
+# coding=utf-8 #Para que no de fallos de caracteres no-ASCII
 # Practica 0 de AAMD
 # Implementación en python de la integración por el metodo 
 # de Monte Carlo
 
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+from matplotlib.backends.backend_agg import FigureCanvasAgg
+from matplotlib.figure import Figure
+
+# create a new figure
+fig = Figure()
+
+# associate fig with the backend
+canvas = FigureCanvasAgg(fig)
+
+# add a subplot to the fig
+#ax = fig.add_subplot(111)
+
+import numpy as np
+
+x = np.arange(-5,5, 0.1)
+y = -x*x #aun falta desplazarlo a la derecha y arriba
+
+plt.figure()
+plt.plot(x, y)
+
+# save the figure to test.png
+canvas.print_png('grafica.png')
 
 def integra_mc (fun, a, b, num_puntos = 1000):
     # Randomizar una lista con 1000 puntos en el segmento a, b 
