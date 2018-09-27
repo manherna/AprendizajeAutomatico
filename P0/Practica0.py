ae2e40be754849eb14903e6cd6918fdf2dba5b10
Manuel Hernández <manherna@ucm.es>
2018-09-27 17:03:09 +0200

    Montecarlo hecho sin vectorizar

--- P0/Practica0.py
+++ P0/Practica0.py
@@ -11,7 +11,7 @@ import scipy as sp
 import scipy.integrate as integ
 
 
-def integra_mc (fun, a, b, num_puntos = 1000):
+def integra_mc (fun, a, b, num_puntos = 10000):
     # Randomizar una lista con 1000 puntos en el segmento a, b 
     # con la función fun. 
     # Después calcular la integral haciendo la división entre
@@ -19,8 +19,8 @@ def integra_mc (fun, a, b, num_puntos = 1000):
     p_x = []
     func = fun
 
-    p_x = np.arange(a, b, (a-b)%10)
 
+    p_x = np.linspace(a,b, 100)
     p_y = [func(n) for n in p_x ]
 
     maxy = max(p_y)
@@ -28,42 +28,44 @@ def integra_mc (fun, a, b, num_puntos = 1000):
     #Randomización de puntos de la función
     xx = []
     yy = []
-    
-    xx = np.array([rnd.randint(0, b) for n in range(0, num_puntos)])
-    yy = np.array([rnd.randint(0, maxy) for n in range(0, num_puntos)])
+
+    xx = np.array([rnd.uniform(0.0, float(b)) for n in range(0, num_puntos)])
+    yy = np.array([rnd.uniform(0.0, maxy) for n in range(0, num_puntos)])
 
     under = 0.0
 
     # Sumamos todos los puntos por debajo de la función
     for x in xx:
-        if (yy[np.where(xx == x)[0][0]] <= func(x)):
+        if (yy[np.where(xx == x)[0][0]] < func(x)):
             under = under+1
 
-
-    print (under)
-    
-    integral = float(under / num_puntos)*(b-a)*maxy
-    print (integral)
-    print (integ.quad(fun, a, b))
-    
 
     plt.figure()
     plt.plot(p_x, p_y, '-')
     plt.plot(xx, yy, 'x')
-    plt.show()
-
-    return 0
 
 
+    return float(under / num_puntos)*(b-a)*maxy
 
 
+
+def foo(x):
+   # return -np.power(float(x), 2)+100
+    return x**2
 
 print("Por favor introduzca el intervalo a integrar A B")
 inp = input()
 a = int(inp.split(' ')[0])
 b = int(inp.split(' ')[1])
+inte = integra_mc(foo, a, b, 1000)
 
-print(str(a), str(b))
 
-integra_mc(lambda a: a*a, a, b, 1000)
+print('Montecarlo: '+ str(inte))
+print('SciPy' + str(integ.quad(foo, a, b)))
+
+plt.show()
+
+
+
+
 
