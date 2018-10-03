fcd0772b3c8166df3e8f01d2f44daa6d9e429138
Manuel Hernández <manherna@ucm.es>
2018-10-03 10:48:01 +0200

    Añadida representación de rendimiento y de integrales respecto a la integral real

--- P0/Practica0.py
+++ P0/Practica0.py
@@ -16,87 +16,101 @@ import time
 def integra_mc (fun, a, b, num_puntos = 10000):
    
     under = 0.0
+    
+    xx = np.array([rnd.uniform(a, float(b)) for n in range(0, num_puntos)])
+    yy = np.array([rnd.uniform(miny, maxy) for n in range(0, num_puntos)])
 
     # Sumamos todos los puntos por debajo de la función
     for x in xx:
         if (yy[np.where(xx == x)[0][0]] < fun(x)):
             under = under+1
 
-
-
-    plt.figure()
-    plt.plot(p_x, p_y, '-')
-    plt.plot(xx, yy, 'x')
-
-
     return float(under / num_puntos)*(b-a)*maxy
 
 def integra_mc_vector(fun, a, b, num_puntos = 10000):
 
     under = 0.0
-
+    xx = np.array([rnd.uniform(a, float(b)) for n in range(0, num_puntos)])
+    yy = np.array([rnd.uniform(miny, maxy) for n in range(0, num_puntos)])
 
     under = sum (1 for x in xx if fun(x) >= yy[np.where(xx == x)[0][0]])
 
-
-    plt.figure()
-    plt.plot(p_x, p_y, '-')
-    plt.plot(xx, yy, 'x')
-
-
     return float(under / num_puntos)*(b-a)*maxy
 
+#Función para calcular el tiempo de ejecucion de func
+def calctiempo(func,integ, nump):
+    tic = time.process_time()
+    integ.append(func(foo, a, b, int(nump)))
+    toc = time.process_time()
+    return toc - tic
 
+#funcion a integrar
 def foo(x):
-   # return -np.power(float(x), 2)+100
-    return -np.power(float(x-100),2) + 10000
+    return -np.power(float(x), 2)+100
+    #return -np.power(float(x-100),2) + 10000
 
-print("Por favor introduzca el intervalo a integrar A B y el numero de puntos")
+
+
+print("Por favor introduzca el intervalo a integrar A B")
 inp = input()
 a = int(inp.split(' ')[0])
 b = int(inp.split(' ')[1])
-numP = int(inp.split(' ')[2])
 fun = foo
 
 # Randomizar una lista con 1000 puntos en el segmento a, b 
 # con la función fun. 
-# Después calcular la integral haciendo la división entre
-# los puntos debajo de la función y fuera
+
 p_x = []
 
 
 p_x = np.linspace(a,b, 10000)
-p_y = [fun(n) for n in p_x ]
+p_y = np.array([fun(n) for n in p_x ])
 
 maxy = max(p_y)
 miny = min(p_y)
 
 
-#Randomización de puntos de la función
-xx = np.array([rnd.uniform(a, float(b)) for n in range(0, numP)])
-yy = np.array([rnd.uniform(miny, maxy) for n in range(0, numP)])
+
+#Arrays para la representación de optimización
+valtemps=[]
+numpunts=np.linspace(100, 1000, 1000)
+inte = []
+intevec = []
+
+realinte = integ.quad(foo, a, b)
 
 
+valtemps = np.array([calctiempo (integra_mc, inte, x) for x in numpunts])
+valtempsvec = np.array([calctiempo (integra_mc_vector, intevec, x) for x in numpunts])
 
 
+print(inte)
 
-tic = 0.0
-toc = 0.0
 
-tic = time.process_time()
-inte = integra_mc(foo, a, b, numP)
-toc = time.process_time()
-print ("NORMAL: "+ str(toc-tic) + " INTEGRAL: " + str(inte))
+#Pintado de resultados
+plt.figure()
 
-tic = time.process_time()
-inte = integra_mc_vector(foo, a, b, numP)
-toc = time.process_time()
-print ("VECTORIZADA: "+ str(toc-tic)+ " INTEGRAL: " + str(inte))
+plt.plot(numpunts, valtemps, '-', label = "Tiempo sin vectorizar", color = "red")
+plt.plot(numpunts, valtempsvec, '-', label = "Tiempo con vectorizacion", color = "green")
+plt.xlabel("Numero de Puntos")
+plt.ylabel("Tiempos de calculo")
+plt.legend()
 
-#print('Montecarlo: '+ str(inte))
-print('SciPy' + str(integ.quad(foo, a, b)))
+plt.figure()
+plt.plot(numpunts, inte, 'x', label = "Integral sin vectorizar")
+plt.plot(numpunts,intevec, 'x', label = "Integral vectorizada")
+plt.plot(numpunts, [realinte[0] for x in numpunts],'-',  color = "red", label = "Integral Real")
+plt.xlabel("Numero de puntos")
+plt.ylabel("Valor Integral")
+plt.legend()
 
 plt.show()
+
+
+
+
+
+
 
 
 
