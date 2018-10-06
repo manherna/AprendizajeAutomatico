75c286b13b5cc2e6e7ce5cce68716743bb1dbadb
mjcguada <mjcguada@gmail.com>
2018-10-06 17:28:54 +0200

    Intentando gradiente con dos variables, aun no se dibuja grafica 3d

--- P1/Practica1.py
+++ P1/Practica1.py
@@ -6,6 +6,7 @@
 import random as rnd
 import matplotlib as mp
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 import scipy as sp
 import time
@@ -17,23 +18,33 @@ def calctiempo(func, tiempos, nump):
     tic = time.process_time()
     #temp =  func(foo, a, b, int(nump))
     toc = time.process_time()
-    tiempos.append(temp)
+    #tiempos.append(temp)
     return toc - tic
 
 #Calcula la función de coste mediante theta, el numero de muestras
 #los vectores de puntos y la función de hipótesis
 def funCoste(muestras, vecX, vecY, fHipo, theta):
     #return sum(((fHipo(theta,vecX)-vecY)**2))/2.0*muestras
-    acum = 0.0
+    acum = 0.0 #Sumatorio
     for i in range(0, muestras):
         acum = acum + (fHipo(theta, vecX[i])-vecY[i])**2
+    
+    return acum/2.0*muestras
+
+# Funcion de coste con dos variables
+def funCoste2(muestras, vecX, vecY, fHipo, theta0, theta1):
+    #return sum(((fHipo(theta,vecX)-vecY)**2))/2.0*muestras
+    acum = 0.0 #Sumatorio
+    for i in range(0, muestras):
+        acum = acum + (fHipo(theta0, theta1, vecX[i])-vecY[i])**2
     
     return acum/2.0*muestras
 
 def funchipo1v(theta, x):
     return theta*x
 
-
+def funchipo2v(theta0, theta1, x):
+    return theta0 + theta1*x
 
 file = open('ex1data1.csv',encoding="utf8",errors='ignore')
 datos = csv.reader(file)
@@ -46,19 +57,32 @@ for x in range(len(lineas)):
     vectorX.append(float(lineas[x][0]))
     vectorY.append(float(lineas[x][1]))
 
-
-tetas = np.arange(-2,5, step = 0.1)
+tetas0 = np.arange(-2, 20, step = 0.1)
+tetas1 = np.arange(0, 10, step = 0.1) #Pendientes (parece que será positiva)
 costes = []
 
-# Da para paja
-# HAy que vectorizar esta mierda
-for i in range (len(tetas)):
-    costes.append(funCoste(len(vectorX), vectorX, vectorY, funchipo1v, tetas[i]))
-    print (funCoste(len(vectorX), vectorX, vectorY, funchipo1v, tetas[i]))
+# Da para paja (Una variable)
+# Hay que vectorizar esta mierda
+#for i in range (len(tetas1)):
+#    costes.append(funCoste(len(vectorX), vectorX, vectorY, funchipo1v, tetas1[i]))
+#    print (funCoste(len(vectorX), vectorX, vectorY, funchipo1v, tetas1[i]))
 
-print (costes)
+#Vamos probando todas las combinaciones
+for i in range(len(tetas0)):
+    for j in range(len(tetas1)):
+        costes.append(funCoste2(len(vectorX), vectorX, vectorY, funchipo2v, tetas0[i], tetas1[j]))
+
+min_coste = min(costes)
+print(min_coste)
+
+#print (costes)
 plt.figure()
-plt.plot(tetas, costes, '-')
+#plt.plot(tetas1, costes, '-')
+fig = plt.figure()
+#Grafica gradiente 3d
+ax3d = fig.add_subplot(111, projection='3d')
+ax3d.plot_wireframe(tetas0, tetas1, costes) #Aqui da fallo
+plt.show()
 
 plt.figure()
 plt.plot(vectorX, vectorY, 'x', color ="red")
@@ -66,6 +90,4 @@ plt.xticks(np.arange(min(vectorX),max(vectorX), step=5))
 plt.yticks(np.arange(min(vectorY), max(vectorY), step=5))
 plt.show()
 
-#plt.plot(vectorX, vectorY, 'x') #esto no, igual cambiando la escala, pero demasiados datos, mejor intervalos
 
-#plt.show()
