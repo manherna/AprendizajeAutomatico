3e04dcada4c24da5e9c552cf2e8d1c39bdab60d5
Manuel Hernández <manherna@ucm.es>
2018-11-21 02:00:50 +0100

    Añadida 1º Parte de la 3º Práctica

--- P3/Practica3.py
+++ P3/Practica3.py
@@ -3,6 +3,7 @@ from scipy.io import loadmat
 import matplotlib as mp
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy.optimize as scopt
 
 #Definicion de funciones ------------------------------------------------------------------
 
@@ -24,6 +25,34 @@ def alg_desGrad(thetas, X, Y):
     H = sigmoide(np.dot(X, thetas))  
     return np.dot((1.0/len(X)), X.T).dot(H-Y)
 
+def alg_desGrad_reg(thetas, X, Y, _lambda):
+    H = sigmoide(np.dot(X, thetas))  
+    return (np.dot((1.0/len(X)), X.T).dot(H-Y))+(_lambda/len(X))*thetas
+
+
+def fun_coste_regularizada(thetas, X, Y, _lambda):
+    H = sigmoide(np.dot(X, thetas))       
+    oper1 = -(float(1)/len(X))
+    oper2 = np.dot((np.log(H)).T, Y)      
+    oper3 = (np.log(1-H)).T                     
+    oper4 = 1-Y
+    oper5 = (_lambda/(2*len(X)))*np.sum(thetas**2)
+    return (oper1 * (oper2 + np.dot(oper3, oper4)))+ oper5
+
+def oneVsAll(X, Y, num_etiquetas, reg):
+    thetas = np.zeros([num_etiquetas, X.shape[1]]) #Thetas es un vector de shape (num_etiquetas, 401)
+
+    for i in range(num_etiquetas):
+        if(i == 0):
+             iaux = 10
+        else:
+            iaux = i
+        
+        a = (Y == iaux)*1
+        thetas[i] = scopt.fmin_tnc(fun_coste_regularizada, thetas[i], alg_desGrad_reg,args = (X, a, reg))[0]
+        
+    return thetas
+
 #Fin de definicion de funciones -----------------------------------------------------------
 
 
@@ -39,12 +68,20 @@ thetas = np.zeros(len(X_unos[0]))
 Y = np.ravel(Y)
 
 
-print(funcoste(thetas, X_unos, Y))
+thetas_opt = oneVsAll(X_unos, Y, 10, 0.1)
+
+resultados = hipotesis(thetas_opt.T, X_unos) #Resultados es un array de (10, 5000) con el resultado de aplicar hipotesis a X
+
+prediccion = resultados.argmax(axis = 1) #Este será un array de (1, 5000) con las posibilidades de que un numero haya sido predecido correctamente
+prediccion[prediccion == 0] = 10
+
+Z = (prediccion == Y)*1
+probabilidad = sum(Z)/len(Y)
+
+print("La probabilidad de acierto del programa es: ", probabilidad*100, "%")
+
+
 #print(Y.shape)
-
-
-
-
 # Para pintar 10 numeros aleatorios de la muestra
 #sample = np.random.choice(X.shape[0], 10)
 #plt.imshow(X[sample, :].reshape(-1, 20).T)
