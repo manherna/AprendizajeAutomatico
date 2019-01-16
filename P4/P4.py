4660e7a883c7b8d41cf2a6d65a0e236267ba2b7a
Manuel Hernández <manherna@ucm.es>
2019-01-16 20:25:47 +0200

    Acercandonos más al coste

--- P4/P4.py
+++ P4/P4.py
@@ -1,7 +1,8 @@
 import displayData
 import numpy as np
+import displayData as dp
 from scipy.io import loadmat
-
+import matplotlib.pyplot as plt
 
 
 # Definicion de funciones -------------------------------------------------------------------------
@@ -9,40 +10,29 @@ def sigmoid(z):
     return 1.0/(1.0 + np.exp(-z))
 
 def frontProp(thetas1, thetas2, X):
-    z2 = thetas1.dot(X).reshape((thetas1.shape[0], 1))
+    z2 = thetas1.dot(X.T)
     a2 = sigmoid(z2)
     z3 = thetas2.dot(a2)
     a3 = sigmoid(z3)
     return a3
-    
+
 
 def backprop(params_rn, num_entradas, num_ocultas, num_etiquetas, X, y, reg):
     theta1 = np.reshape(params_rn[:num_ocultas *(num_entradas + 1)],(num_ocultas, (num_entradas+1)))
     theta1 = np.insert(theta1,0,1,axis = 0) # Theta1 es un array de num_ocultas +1, num_entradas
     # theta2 es un array de (num_etiquetas, num_ocultas)
     theta2 = np.reshape(params_rn[num_ocultas*(num_entradas + 1): ], (num_etiquetas,(num_ocultas+1)))
-
     # Array con los thetas de la red.
     # Dimensiones (num_entradas, num_etiquetas)
     X_unos = np.hstack([np.ones((len(X), 1), dtype = np.int), X])
-
     totalcost = 0.0
-    for i in range(nMuestras):
-        caseRow = X_unos [i]
-        caseY  = np.reshape(y[i], (1, len(y[i])))
-        hipo = frontProp(theta1, theta2, caseRow)
-        casecost = -(caseY).dot(np.log(hipo)) - (1-caseY).dot(np.log(1- hipo))
-        totalcost = totalcost + casecost
+    hipo = frontProp(theta1, theta2, X_unos)
+    muestras = y.shape[0]
 
-    totalcost = totalcost /nMuestras
+    cost = np.sum((-y.T)*(np.log(hipo)) - (1-y.T)*(np.log(1- hipo)))/muestras
+    print(cost)
+    return cost
 
-    return float(totalcost)
-
-
-
-   
-   
-    
 
 def getYMatrix(Y, nMuestras, nEtiquetas):
     nY =  np.zeros((nMuestras, nEtiquetas))
@@ -61,6 +51,11 @@ X = data['X']  # Cada fila de X representa una escala de grises de 20x20 despleg
 nMuestras = len(X)
 Y = np.ravel(Y)
 Y_Mat = getYMatrix(Y,nMuestras, (max(Y)-min(Y))+1)
+
+print(X.shape)
+print(Y.shape)
+print(Y_Mat.shape)
+
 
 weights = loadmat('ex4weights.mat')
 theta1, theta2 = weights['Theta1'], weights ['Theta2']
