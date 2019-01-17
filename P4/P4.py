12b2cc659a2aa38a11d97d1c0e47b992f12228c8
Manuel Hernández <manherna@ucm.es>
2019-01-17 13:15:59 +0100

    Comenzada backprop

--- P4/P4.py
+++ P4/P4.py
@@ -19,6 +19,12 @@ def frontProp(thetas1, thetas2, X):
     a3 = sigmoid(z3)
     return a3
 
+def costFun(X, y, theta1, theta2,  reg):
+    hipo = frontProp(theta1, theta2, X)
+    muestras = y.shape[0]
+    cost = np.sum((-y.T)*(np.log(hipo)) - (1-y.T)*(np.log(1- hipo)))/muestras
+    regcost = ((np.sum(theta1**2)+np.sum(theta2**2))*reg)/(2*muestras)
+    return cost + regcost
 
 def backprop(params_rn, num_entradas, num_ocultas, num_etiquetas, X, y, reg):
     theta1 = np.reshape(params_rn[:num_ocultas *(num_entradas + 1)],(num_ocultas, (num_entradas+1)))
@@ -28,14 +34,23 @@ def backprop(params_rn, num_entradas, num_ocultas, num_etiquetas, X, y, reg):
     # Array con los thetas de la red.
     # Dimensiones (num_entradas, num_etiquetas)
     X_unos = np.hstack([np.ones((len(X), 1), dtype = np.int), X])
-    hipo = frontProp(theta1, theta2, X_unos)
-    muestras = y.shape[0]
+    cost = costFun(X_unos, y, theta1, theta2, reg)
 
-    cost = np.sum((-y.T)*(np.log(hipo)) - (1-y.T)*(np.log(1- hipo)))/muestras
-    regcost = ((np.sum(theta1**2)+np.sum(theta2**2))*reg)/(2*muestras)
+    deriv = sigmoidDerivative(theta1.dot(X_unos.T))
+    print(deriv.shape)
 
-    totalcost = cost + regcost
-    return totalcost
+    for t in range (len(y)):
+        caseX = X_unos[t]
+        caseY = y[t]
+        hipo = frontProp(theta1, theta2, caseX)
+        delta3 = hipo - caseY
+        delta2 = theta2.T.dot(delta3).dot(deriv)
+        if(t == 1 ): 
+            print(delta3.shape)
+            print(delta2.shape)
+    
+
+    return cost
 
 
 def getYMatrix(Y, nMuestras, nEtiquetas):
@@ -66,8 +81,7 @@ Y_Mat = getYMatrix(Y,nMuestras, (max(Y)-min(Y))+1)
 weights = loadmat('ex4weights.mat')
 theta1, theta2 = weights['Theta1'], weights ['Theta2']
 
-th = weightInitialize(2, 3)
-print(th)
+
 
 params = np.hstack((np.ravel(theta1), np.ravel(theta2)))
 print(backprop(params,X.shape[1], 25, 10, X, Y_Mat, 0.7))
