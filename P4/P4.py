56db3f36e037f92575f9e6a18ee4ede9045201b7
Manuel Hernández <manherna@ucm.es>
2019-01-17 12:24:56 +0100

    Función de coste implementada.

--- P4/P4.py
+++ P4/P4.py
@@ -9,6 +9,9 @@ import matplotlib.pyplot as plt
 def sigmoid(z):
     return 1.0/(1.0 + np.exp(-z))
 
+def sigmoidDerivative(z):
+    return sigmoid(z)*(1-sigmoid(z))
+
 def frontProp(thetas1, thetas2, X):
     z2 = thetas1.dot(X.T)
     a2 = sigmoid(z2)
@@ -25,23 +28,30 @@ def backprop(params_rn, num_entradas, num_ocultas, num_etiquetas, X, y, reg):
     # Array con los thetas de la red.
     # Dimensiones (num_entradas, num_etiquetas)
     X_unos = np.hstack([np.ones((len(X), 1), dtype = np.int), X])
-    totalcost = 0.0
     hipo = frontProp(theta1, theta2, X_unos)
     muestras = y.shape[0]
 
     cost = np.sum((-y.T)*(np.log(hipo)) - (1-y.T)*(np.log(1- hipo)))/muestras
-    print(cost)
-    return cost
+    regcost = ((np.sum(theta1**2)+np.sum(theta2**2))*reg)/(2*muestras)
+
+    totalcost = cost + regcost
+    return totalcost
 
 
 def getYMatrix(Y, nMuestras, nEtiquetas):
     nY =  np.zeros((nMuestras, nEtiquetas))
-    j = 0
-    for i in Y:
-        if (i == 10): i = 0
-        nY[j][i] = 1
-        j = j+1
+    yaux = Y -1
+    
+    for i in range(len(nY)):
+        nY[i][yaux[i]] = 1
     return nY
+
+#returns an initialized matrix of (1+ L_in, L_out)
+def weightInitialize(L_in, L_out):
+    cini = 0.12
+    aux = np.random.uniform(-cini, cini, size =(L_in, L_out))
+    aux = np.insert(aux,0,1,axis = 0)
+    return aux
 
 
 # Fin funciones -----------------------------------------------------------------------------------
@@ -52,14 +62,12 @@ nMuestras = len(X)
 Y = np.ravel(Y)
 Y_Mat = getYMatrix(Y,nMuestras, (max(Y)-min(Y))+1)
 
-print(X.shape)
-print(Y.shape)
-print(Y_Mat.shape)
-
 
 weights = loadmat('ex4weights.mat')
 theta1, theta2 = weights['Theta1'], weights ['Theta2']
 
+th = weightInitialize(2, 3)
+print(th)
 
 params = np.hstack((np.ravel(theta1), np.ravel(theta2)))
-print(backprop(params,X.shape[1], 25, 10, X, Y_Mat, 0.1))
+print(backprop(params,X.shape[1], 25, 10, X, Y_Mat, 0.7))
